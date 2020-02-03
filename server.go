c903d1b69ad45a57fc603a3496ab08d879ffdf21
jadtaphon <jadtaphon.ch.59@ubu.ac.th>
2020-02-03 13:36:04 +0700

    test02v.2

--- server.go
+++ server.go
@@ -14,7 +14,7 @@ func main() {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 	e.GET("/gettest",getUser)
-	e.Logger.Fatal(e.Start(":8080"))
+	e.Logger.Fatal(e.Start(":80"))
 }
 
 func getUser(c echo.Context) error {
