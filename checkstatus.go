e51ba7237ca030a6ee9129eb2d72bd7e193cfc25
jadtaphon <jadtaphon.ch.59@ubu.ac.th>
2020-02-08 21:13:39 +0700

    wockinggit add .git add .git add .git add .

--- checkstatus.go
+++ checkstatus.go
@@ -1,49 +1,81 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo/v4"
-	"gopkg.in/mgo.v2/bson"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	//"gopkg.in/mgo.v2/bson"
 
 )
 
 func (h *Handler) getUser(c echo.Context) (err error) {
-	users := []*DataQR{}
-	db := h.DB.Clone()
+	// users := []*DataQR{}
 
-	if err = db.DB("test").C("data_T").Find(nil).All(&users); err != nil {
-		return c.JSON(http.StatusBadRequest, err)
+	id := c.Param("id")
+	db, err := mongo.NewClient(options.Client().ApplyURI(h.URL))
+	if err != nil {
+		log.Fatal(err)
 	}
-	defer db.Close()
-	return c.JSON(http.StatusOK, &users)
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = db.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Disconnect(ctx)
+
+	objID, _ := primitive.ObjectIDFromHex(id)
+	cursor, err := db.Database("test").Collection("qr_api").Find(ctx, bson.M{"_id": objID})
+	if err != nil {
+		log.Fatal(err)
+	}
+	var qrury []bson.M
+	if err = cursor.All(ctx, &qrury); err != nil {
+		log.Fatal(err)
+	}
+	log.Println(id)
+
+	// defer db.Close()
+	return c.JSON(http.StatusOK, qrury)
 }
 
 func (h *Handler) createqr(c echo.Context) (err error) {
 
-	user := &DataQR{ID: bson.NewObjectId()}
-	err = c.Bind(user)
+	status := c.FormValue("status")
+	idcoure := c.FormValue("idcoure")
+
+	db, err := mongo.NewClient(options.Client().ApplyURI(h.URL))
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, err.Error())
+		log.Fatal(err)
 	}
-
-	///////////////////////////////
-	db := h.DB.Clone()
-	defer db.Close()
-
-	if err = db.DB("test").C("data_T").Insert(user); err != nil {
-		return c.JSON(http.StatusBadRequest, err)
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = db.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
 	}
-	return c.JSON(http.StatusOK, user)
+	defer db.Disconnect(ctx)
+
+	update, err := db.Database("test").Collection("qr_api").InsertOne(ctx, bson.D{
+		{Key: "status", Value: status},
+		{Key: "idcoure", Value: idcoure},
+	})
+
+	return c.JSON(http.StatusOK, update)
 }
 
-// func (u *Handler) checkkey(c echo.Context) (err error)  {
-// 	key:= c.Param("id")
-// 	keyqr, err := strconv.Atoi(key)
-// 	// if u.Data.Key==keyqr {
-// 	// 	return c.JSON(http.StatusOK, u.Status)
-// 	// }
-// 	log.Println(key)
-// 	log.Println(keyqr)
-// 		return c.JSON(http.StatusOK, u)
-// }
+// // func (u *Handler) checkkey(c echo.Context) (err error)  {
+// // 	key:= c.Param("id")
+// // 	keyqr, err := strconv.Atoi(key)
+// // 	// if u.Data.Key==keyqr {
+// // 	// 	return c.JSON(http.StatusOK, u.Status)
+// // 	// }
+// // 	log.Println(key)
+// // 	log.Println(keyqr)
+// // 		return c.JSON(http.StatusOK, u)
+// // }
--- server.go
+++ server.go
@@ -1,43 +1,58 @@
-
 package main
 
 import (
 	"net/http"
 	"os"
 
-	//"github.com/labstack/echo/middleware"
 	"github.com/labstack/echo/v4"
-	"gopkg.in/mgo.v2"
 
 )
 
 func main() {
 
-	port:= os.Getenv("PORT")
+	port := os.Getenv("PORT")
 	e := echo.New()
-	// e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-	// 	AllowOrigins: []string{"*"},
-	// 	AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
-	// }))
 
-	// db, err := mgo.Dial("localhost")
+	url := "mongodb+srv://jadtaphon:hbrY7322@cluster0-vkyg7.mongodb.net/test?retryWrites=true&w=majority"
 
-	db, err := mgo.Dial("localhost")
-	if err != nil {
-		e.Logger.Fatal(err)
-	}
-
-	// hub := newHub()
-	// go hub.run()
-
-	h := &Handler{DB: db}
+	h := &Handler{URL: url}
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 	e.POST("/createqr", h.createqr)
 	// e.GET("check_key/:id", h.checkkey)
-	e.GET("/gettest",h.getUser)
-	e.Logger.Fatal(e.Start(":"+port))
+	e.GET("/gettest/:id", h.getUser)
+	e.Logger.Fatal(e.Start(":" + port))
 }
 
+///////////database connecttion//////////////
+// db, err := mongo.NewClient(options.Client().ApplyURI(url))
+// if err != nil {
+// 	log.Fatal(err)
+// }
+// ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+// err = db.Connect(ctx)
+// if err != nil {
+// 	log.Fatal(err)
+// }
+// defer db.Disconnect(ctx)
+// err = client.Ping(ctx, readpref.Primary())
+// if err != nil {
+// 	log.Fatal(err)
+// }
 
+// quickstartDatabase := db.Database("test")
+// testCollection := quickstartDatabase.Collection("qr_api")
+
+// update, err := testCollection.InsertOne(ctx, bson.D{
+// 	{Key: "status", Value: "true"},
+// })
+// log.Println(quickstartDatabase)
+// log.Println(update.InsertedID)
+// log.Println(db)
+
+// err = client.Ping(db, readpref.Primary())
+// if err != nil {
+// 	log.Fatal(err)
+// }
+//////////////////////////////////////////////////////////////
