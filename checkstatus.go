785e34e5292073438ad69c9c231bb81e943d3599
jadtaphon <33340871+jadtaphon@users.noreply.github.com>
2020-02-17 16:38:47 +0700

    Update checkstatus.go

--- checkstatus.go
+++ checkstatus.go
@@ -20,7 +20,7 @@ func (h *Handler) getALL(c echo.Context) (err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	ctx, _ := context.WithTimeout(context.Background(), 17*time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), 19*time.Second)
 	err = db.Connect(ctx)
 	if err != nil {
 		log.Fatal(err)
