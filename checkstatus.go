93901ae7f3c90074e5eb9085b402726c0c6cda6b
jadtaphon <33340871+jadtaphon@users.noreply.github.com>
2020-02-17 15:06:07 +0700

    Update checkstatus.go

--- checkstatus.go
+++ checkstatus.go
@@ -16,18 +16,18 @@ import (
 )
 
 func (h *Handler) getALL(c echo.Context) (err error) {
-// 	db, err := mongo.NewClient(options.Client().ApplyURI(h.URL))
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-// 	err = db.Connect(ctx)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	defer db.Disconnect(ctx)
+	db, err := mongo.NewClient(options.Client().ApplyURI(h.URL))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = db.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Disconnect(ctx)
 
-// 	qrury, err := db.Database("testAPL").Collection("qr_api").Find(ctx, bson.M{})
+	qrury, err := db.Database("testAPL").Collection("qr_api").Find(ctx, bson.M{})
 // 	var result []bson.M
 // 	if err = qrury.All(ctx, &result); err != nil {
 // 		log.Fatal(err)
@@ -35,7 +35,7 @@ func (h *Handler) getALL(c echo.Context) (err error) {
 	log.Println(h.URL)
 	//result:="jadtaphon"
 
-	return c.JSON(http.StatusOK, h.URL)
+	return c.JSON(http.StatusOK, qrury)
 }
 
 //////////////////////////////////////////////////////////////////////////////////////
