63b32b8f0d38e8ace2ca6aa2387c9b69339db603
jadtaphon <jadtaphon.ch.59@ubu.ac.th>
2020-02-20 19:31:03 +0700

    19/2-v9.6

--- checkstatus.go
+++ checkstatus.go
@@ -38,8 +38,8 @@ func (h *Handler) getKey(c echo.Context) (err error) {
 		return c.JSON(http.StatusBadRequest, err)
 	}
 	//log.Println(users.Url)
-	return c.JSON(http.StatusOK, users.Url)
-	//return c.Redirect(http.StatusMovedPermanently, users.Url)
+	//return c.JSON(http.StatusOK, users.Url)
+	return c.Redirect(http.StatusMovedPermanently, users.Url)
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
